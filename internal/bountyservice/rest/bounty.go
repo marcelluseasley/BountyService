4e44da6ff9a921a60f6717a63c3fb4bcdc1abc74
Marcellus Easley <marcellus.easley@gmail.com>
2024-06-24 00:21:32 -0400

    add bounty create request

---
+++ internal/bountyservice/rest/bounty.go
@@ -0,0 +1,47 @@
+package rest
+
+import (
+	"encoding/json"
+	
+	"net/http"
+	"strconv"
+	"time"
+
+	db "bounty-poc/internal/bountyservice/repo/postgres"
+	"bounty-poc/internal/bountyservice/service"
+
+
+)
+
+type BountyRequest struct {
+	ID          int       `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Reward      string    `json:"reward"` // stars:2 or coins: 1 or currency:20.00
+	Category    string    `json:"category"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Deadline    time.Time `json:"deadline"`
+	CreatorID   int       `json:"creator_id"`
+	AssigneeID  int       `json:"assignee_id"`
+}
+
+
+func CreateBounty(w http.ResponseWriter, r *http.Request) {
+
+	bounty := &db.Bounty{}
+	// get body from request and store in object of model bounty
+	err := json.NewDecoder(r.Body).Decode(&bounty)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	createdID, err := service.CreateBounty(bounty)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(strconv.Itoa(createdID)))
+}
+
